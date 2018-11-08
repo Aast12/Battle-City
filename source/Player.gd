ffd54e2bebf96cecef844dbedbd061fcba6e7547
Alam Sanchez <asancheztorres1905@gmail.com>
2018-11-07 18:34:49 -0600

    Add player aim

--- source/Player.gd
+++ source/Player.gd
@@ -15,6 +15,7 @@ func _ready():
 
 func get_input():
 	movement = Vector2()
+	var mouse_pos = get_viewport().get_mouse_position() - global_position
 	if Input.is_key_pressed(KEY_W):
 		movement.y -= 1
 	if Input.is_key_pressed(KEY_D):
@@ -23,11 +24,37 @@ func get_input():
 		movement.y += 1
 	if Input.is_key_pressed(KEY_A):
 		movement.x -= 1
-	movement = movement.normalized() * speed
-	
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		if can_shoot:
 			shoot(get_viewport().get_mouse_position())
+	
+	movement = movement.normalized() * speed
+	
+	if movement.length() > 0:
+		$Sprite.play()
+	else:
+		$Sprite.stop()
+	
+	if mouse_pos.x > -50 and mouse_pos.x < 50:
+		if mouse_pos.y > 0:
+			$Sprite.animation = "down"
+		else:
+			$Sprite.animation = "up"
+	elif mouse_pos.y > -50 and mouse_pos.y < 50:
+		if mouse_pos.x > 0:
+			$Sprite.animation = "right"
+		else:
+			$Sprite.animation = "left"
+	else:
+		if mouse_pos.x > 0 and mouse_pos.y < 0:
+			$Sprite.animation = "up right"
+		elif mouse_pos.x > 0 and mouse_pos.y > 0:
+			$Sprite.animation = "down right"
+		elif mouse_pos.x < 0 and mouse_pos.y < 0:
+			$Sprite.animation = "up left"
+		else:
+			$Sprite.animation = "down left"
+
 
 func shoot(pos):
 	var bullet = Bullet.instance()
@@ -40,6 +67,7 @@ func shoot(pos):
 func _physics_process(delta):
 	get_input()
 	movement = move_and_slide(movement)
+	print(get_viewport().get_mouse_position().y)
 
 func _on_ShootTimer_timeout():
 	can_shoot = true
