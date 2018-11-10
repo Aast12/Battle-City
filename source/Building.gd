80cc844195a7080b55d5ea14204c8c6132269980
Enrique Perez <a00824707@itesm.mx>
2018-11-09 21:54:49 -0600

    Hospital Building

--- source/Building.gd
+++ source/Building.gd
@@ -1,6 +1,16 @@
 extends StaticBody2D
 
-func _on_Area2D_body_entered(body):
-	if Input.is_key_pressed(KEY_Z):
-		print ("Cagajo")
-	print(2)
+var player
+var is_healing = false
+var heal_factor = 1
+var hospital_health = 0 
+
+func _on_Area2D_body_entered(body):	
+	if body.id == "player":
+		player = body
+		if Input.is_key_pressed(KEY_Z):
+			is_healing = true
+
+func _process(delta):
+	if player and is_healing and hospital_health + player.max_hp * 0.75 <= player.max_hp * 0.75:
+		player.hp = min(player.hp + heal_factor * delta, player.max_hp)
