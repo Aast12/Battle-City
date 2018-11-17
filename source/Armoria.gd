a7012dea7334bfa4b4402d80545207daeb4eab1e
Enrique Perez <a00824707@itesm.mx>
2018-11-16 21:01:21 -0600

    Armoria Actualizado

--- source/Armoria.gd
+++ source/Armoria.gd
@@ -1,17 +1,27 @@
 extends StaticBody2D
 
+var id = "Armeria"
 var player
 var is_healing = false
 var heal_factor = 1
 var hospital_health = 0 
+var rate_factor = 2
 
 func _on_Area2D_body_entered(body):	
 	if body.id == "player":
 		player = body
 		if Input.is_key_pressed(KEY_Z):
 			is_healing = true
+			
 
 func _process(delta):
-	if player and is_healing and hospital_health <= player.max_hp * 0.75:
-		hospital_health += heal_factor * delta
-		player.hp = min(player.hp + heal_factor * delta, player.max_hp)
+	if player and is_healing:
+		timer.start()
+		player.fire_rate = player.fire_rate * rate_factor
+		
+
+func _on_Timer_timeout():
+	player.fire_rate = player.fire_rate/rate_factor
+	
+func _ready():
+	$timer.wait_time = 5
