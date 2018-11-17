1f235f74232f3403d1743789a20e97e2bb7d69c1
Enrique Perez <a00824707@itesm.mx>
2018-11-16 19:43:14 -0600

    add Bank and Soda buldings

---
+++ source/Banco.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+var player
+var is_healing = false
+var heal_factor = 1
+var banco_health = 10 
+var cash
+
+func _on_Area2D_body_entered(body):	
+	if body.id == "player":
+		player = body
+		if Input.is_key_pressed(KEY_Z):
+			is_healing = true
+
+func _process(delta):
+	if player and is_healing and banco_health > 0:
+		banco_health += heal_factor * delta
+		player.cash = min(player.cash + heal_factor * delta, player.max_cash)
---
+++ source/Soda.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+var player
+var is_healing = false
+var heal_factor = 1
+var hospital_health = 0 
+var stamina
+var max_stamina
+
+func _on_Area2D_body_entered(body):	
+	if body.id == "player":
+		player = body
+		if Input.is_key_pressed(KEY_Z):
+			is_healing = true
+
+func _process(delta):
+	if player and is_healing:
+		player.stamina = min(player.stamina + heal_factor * delta, player.max_stamina)
+
