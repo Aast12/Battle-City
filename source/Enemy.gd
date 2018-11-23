f612ba2ea15d2de348b670ae5c725e8da37f1f76
Alam Sanchez <asancheztorres1905@gmail.com>
2018-11-23 08:31:04 -0600

    Add sniper enemy type

--- source/Enemy.gd
+++ source/Enemy.gd
@@ -3,23 +3,30 @@ extends KinematicBody2D
 var player
 var id = "enemy"
 var hp = 75
-var speed = 300
+var speed = 150
 var movement = Vector2()
 var atk_rate = 1.2
 var can_attack = true
 var target
+var pushable = true;
 
 func _ready():
 	$AttackTimer.wait_time = atk_rate
+	$AnimatedSprite.play()
 
 func init(target, pos):
 	player = target
 	position = pos
 
+func push_movement(vector, delta):
+	var collision = move_and_collide(vector * delta)
+	if collision and collision.collider.pushable:
+		collision.collider.push_movement(vector, delta)
+
 func _physics_process(delta):
 	movement = (player.global_position - global_position)
 	movement = movement.normalized() * speed
-	rotation = (player.global_position - global_position).angle() + PI / 2
+	rotation = (player.global_position - global_position).angle() + PI / 2 + PI
 	move_and_slide(movement)
 	if hp <= 0:
 		queue_free()
--- source/Game.gd
+++ source/Game.gd
@@ -1,20 +1,38 @@
 extends Node
 
 export (PackedScene) var Enemy
-
+export (PackedScene) var Sniper
 #var enemies = []
 #var wr = []
 
+var arrow = load("res://Art/aim.png")
+#var beam = load("res://beam.png")
+
 func _ready():
 	for i in range(10):
-		var enemy_pos = Vector2(rand_range(-400, 1000), rand_range(-300, 700))
+		var enemy_pos = Vector2(rand_range(-1000, 1000), rand_range(-1000, 1000))
 		var enemy = Enemy.instance()
 		enemy.init($Player, enemy_pos)
 		#wr = weakref(enemy)
 		add_child(enemy)
 		#128, 151, 188
+	for i in range(10):
+		var enemy_pos = Vector2(rand_range(-1000, 1000), rand_range(-1000, 1000))
+		var enemy = Sniper.instance()
+		enemy.init($Player, enemy_pos)
+		#wr = weakref(enemy)
+		add_child(enemy)
+		#128, 151, 188
 	var night = Color(128.0 / 255, 151.0 / 255, 188.0 / 255, 1)
-	$Player.get_node("Sprite").modulate = night.lightened(0)
+	$CanvasModulate.color = night
+	#$Player.get_node("Sprite").modulate = night.lightened(0)
+	
+	# Changes only the arrow shape of the cursor
+	# This is similar to changing it in the project settings
+	Input.set_custom_mouse_cursor(arrow)
+
+	# Changes a specific shape of the cursor (here the IBeam shape)
+	#Input.set_custom_mouse_cursor(beam, Input.CURSOR_IBEAM)
 
 func _process(delta):
 	pass
